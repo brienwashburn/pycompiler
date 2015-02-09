ef6d4987cf7b320d23c4711a788d623a61e1bbf4
timbo24 <tkkemo@gmail.com>
2015-02-09 01:36:30 -0700

    stubbed out definitions for string literal lexer, not sure if needed

--- stringlitlexer.rkt
+++ stringlitlexer.rkt
@@ -1,1 +1,28 @@
 #lang racket
+
+(require parser-tools/lex)
+(require (prefix-in : parser-tools/lex-sre))
+
+(define-lex-abbrev stringliteral)
+(define-lex-abbrev stringprefix)
+(define-lex-abbrev shortstring)
+(define-lex-abbrev longstring)
+(define-lex-abbrev shortstringitem)
+(define-lex-abbrev longstringitem)
+(define-lex-abbrev shortstringchar)
+(define-lex-abbrev longstringchar)
+(define-lex-abbrev stringescapeseq)
+
+(define-lex-abbrev bytesliteral)
+(define-lex-abbrev bytesprefix)
+(define-lex-abbrev shortbytes)
+(define-lex-abbrev longbytes)
+(define-lex-abbrev shortbytesitem)
+(define-lex-abbrev longbytesitem)
+(define-lex-abbrev shortbyteschar)
+(define-lex-abbrev longbyteschar)
+(define-lex-abbrev bytesescapeseq (:: #\\ (char-range #\u0 #\u127)))
+
+
+(define string-lexer
+  (lexer
