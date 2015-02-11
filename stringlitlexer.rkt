62d1dd6668a7a1c84ae207044310c45b1eb8769c
timbo24 <tkkemo@gmail.com>
2015-02-10 21:47:19 -0700

    string literal lexer near completion, need to handle special name escape chars

--- stringlitlexer.rkt
+++ stringlitlexer.rkt
@@ -5,11 +5,13 @@
 
 (define-lex-abbrev stringliteral (:: (repetition 0 1 stringprefix) (union shortstring longstring)))
 (define-lex-abbrev stringprefix (union #\r #\u #\R #\U))
-(define-lex-abbrev shortstring (union (:: #\' (:* shortstringitem) #\') (:: #\" (:* shortstringitem) #\")))
+(define-lex-abbrev shortstring (union (:: #\' (:* shortstringitemsinglequote) #\') (:: #\" (:* shortstringitemdoublequote) #\")))
 (define-lex-abbrev longstring (union (:: #\' #\' #\' (:* longstringitem) #\' #\' #\') (:: #\" #\" #\" (:* longstringitem) #\" #\" #\")))
-(define-lex-abbrev shortstringitem (union shortstringchar stringescapeseq))
+(define-lex-abbrev shortstringitemsinglequote (union shortstringcharsinglequote stringescapeseq))
+(define-lex-abbrev shortstringitemdoublequote (union shortstringchardoublequote stringescapeseq))
 (define-lex-abbrev longstringitem (union longstringchar stringescapeseq))
-(define-lex-abbrev shortstringchar (intersection (char-range #\u0 #\u127) (char-complement #\\) (char-complement #\newline) (char-complement #\")))
+(define-lex-abbrev shortstringcharsinglequote (intersection (char-range #\u0 #\u127) (char-complement #\\) (char-complement #\newline) (char-complement #\')))
+(define-lex-abbrev shortstringchardoublequote (intersection (char-range #\u0 #\u127) (char-complement #\\) (char-complement #\newline) (char-complement #\"ZZZZ)))
 (define-lex-abbrev longstringchar (intersection any-char (char-complement #\\)))
 (define-lex-abbrev stringescapeseq (:: #\\ any-char))
 
@@ -23,9 +25,21 @@
 (define-lex-abbrev longbyteschar (intersection (char-range #\u0 #\u127) (char-complement #\\)))
 (define-lex-abbrev bytesescapeseq (:: #\\ (char-range #\u0 #\u127)))
 
+(define-lex-abbrev quote-markers (union #\" #\' (:: #\" #\" #\") (:: #\' #\' #\')))
+
+(define-lex-abbrev unicode-name (:: #\\ #\N #\{ (:+ (intersection any-char (char-complement #\}))) #\}))
+
+
 
 (define (unget port length)
   (file-position port (- (file-position port) length)))
+
+(define (advance port length)
+  (file-position port (+ (file-position port) length)))
+
+
+(define closing-seq "")
+(define string-mode "")
 
 
 (define string-lexer1
@@ -36,11 +50,36 @@
    [bytesliteral 'dolly]
    [any-char 'nope]))
 
-(define string-lexer-baby
+(define initial-string-lexer
     (lexer
-     [#\" (string-lexer-baby input-port)]
-     [(:* shortstringitem) 'the]
-     [(:* any-char) 'fuck]))
+     [quote-markers (begin
+                      (set! current-closing-seq lexeme)
+                      (cond
+                        [(equal? string-mode "r") (raw-string-lexer input-port)]
+                        [else (normal-string-lexer input-port)]))]
+     [stringprefix (cond
+                     [(equal? (string-downcase lexeme) "r") (begin 
+                                                              (set! string-mode "r")
+                                                              (initial-string-lexer input-port))]
+                     [else (initial-string-lexer input-port)])])) 
+
+(define raw-string-lexer
+  (lexer
+   [(:: (intersection (:* any-char) (complement closing-seq)))
+    (begin
+      (advance input-port (string-length closing-seq))
+      (cons (list 'LIT lexeme)
+            (basic-lexer input-port)))]))
+
+
+(define (normal-lexer port literal)
+  (define normal-lexer-inside
+    (lexer
+     [unicode-name 
+      (begin
+        (define thang (SEARCH A DICTIONARY (string-downcase (substring lexeme 3 (- (length lexeme) 1)))))
+        (string? thang)
+        ]
 
 (define (someshit port)
   'whatthefuck)
